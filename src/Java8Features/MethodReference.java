2f0d78718a38e1af1b746615489d88b70b94d93f
Your Name <dhianeshwar>
2024-01-08 10:35:36 +0530

    adding another file

---
+++ src/Java8Features/MethodReference.java
@@ -0,0 +1,20 @@
+package Java8Features;
+interface Parse
+{
+    int findElement(int[] nums,int num);
+}
+public class MethodReference {
+    public static void main(String[] args) {
+        Parse p = (int[] nums, int num) -> {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == num) {
+                    num = i;
+                    break;
+                }
+            }
+            return num;
+        };
+        int[] arr = {1, 2, 3};
+        System.out.println(p.findElement(arr, 3));
+    }
+}
