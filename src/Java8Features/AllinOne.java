48779bb5e32d09a308f9c0a70f851d4fdc016d0e
Your Name <dhianeshwar>
2024-01-08 10:32:56 +0530

    adding a file in Java 8 feature

---
+++ src/Java8Features/AllinOne.java
@@ -0,0 +1,45 @@
+package Java8Features;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+//-----------------------------Lombok--------------------------------------
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+class StudentDetails
+{
+    private String name;
+    private int rollNo;
+    private int age;
+}
+
+public class AllinOne {
+    public static void main(String[] args) {
+        List<StudentDetails> list=new ArrayList<>();
+        list.add(new StudentDetails("santosh",1,18));
+        list.add(new StudentDetails("kumar",2,19));
+        list.add(new StudentDetails("raja",3,30));
+        list.add(new StudentDetails("senthil",4,45));
+        list.add(new StudentDetails("roshan",5,15));
+        //---------------------Functional Interface-------------------------------
+        //---------------------Method Reference----------------------------
+        Comparator<StudentDetails> nameComparator= Comparator.comparing(StudentDetails::getName);
+        list.sort(nameComparator);
+
+        //----------------------------Streams-------------------------------------
+        AtomicInteger i= new AtomicInteger(1);
+        list.forEach(student->student.setRollNo(i.getAndIncrement()));
+
+        list.forEach(System.out::println);
+
+    }
+}
